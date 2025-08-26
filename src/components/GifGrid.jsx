a82da1a18531e38d9caff2c0f2e6938d12d6a0e2
Jhonatan Romero <n.bjhon@gmail.com>
2025-08-25 23:24:09 -0500

    add url of Gift

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from "react";
+import { getGifts } from "../helpers/getGifs";
 
 
 function GifGrid({ category }) {
     
+    const [counter, setcounter] = useState(10);
+    useEffect( ()=> {
+        getGifts(category);
+
+        
+    }, [])
+
     
-    
+
     return (
         <>
             <h3>{ category }</h3>    
-            
+            <h5>{ counter }</h5>
+            <button onClick={()=> setcounter(counter + 1)}>+1</button>
         </>
     )
 }
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,16 @@
+export const getGifts = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=gDZkeTYcWBjv5i8NBTY6bIibc0OKp5Mn&q=${ category }&limit=10`;
+    const resp = await fetch( url );
+    const { data =[] } = await resp.json();
+
+    const gifs = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }))
+
+    console.log(gifs);
+    return gifs;
+        
+}
